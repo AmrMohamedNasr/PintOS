0efe913f6ea07661260adc2bafe77fd402593116
root <aya_aly_abouzeid@yahoo.com>
2017-11-22 22:05:37 +0200

    Remove Busy Waiting #1

--- src/devices/timer.h
+++ src/devices/timer.h
@@ -3,9 +3,12 @@
 
 #include <round.h>
 #include <stdint.h>
+#include <list.h>
 
 /* Number of timer interrupts per second. */
 #define TIMER_FREQ 100
+
+struct list busy_waiters;
 
 void timer_init (void);
 void timer_calibrate (void);
