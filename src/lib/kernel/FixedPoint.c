0ed5116bff66392b9b4d3e2c781934538cd094a8
Michael <micoraafat96@gmail.com>
2017-11-23 18:22:10 +0200

    modifying fixed point

--- src/lib/kernel/FixedPoint.c
+++ src/lib/kernel/FixedPoint.c
@@ -4,10 +4,25 @@
  fixed_point fixed_point_mul(fixed_point x, fixed_point y) {
     // right shift 14 bits after multiplication.
     //to return correct fixed point representation of result.
-    return ( x  *  y ) >> 14;
+    return ( x  *  y ) >> Fixed_Point_FBits;
 }
  fixed_point fixed_point_div(fixed_point x, fixed_point y) {
     // left shift 14 bits before division.
     // to void error and it returns the correct representation of fixed point.
-    return (  x << 14 ) / y  ;
+    return (  x << Fixed_Point_FBits ) / y  ;
+}
+
+fixed_point integer_to_fixed_point(int x) {
+    return x << Fixed_Point_FBits;
+}
+
+int fixed_point_to_integer(fixed_point x) {
+    return x >> Fixed_Point_FBits;
+}
+
+int round_fixed_point_number_to_integer(fixed_point x) {
+    if ( (x >> 14) >= 0 ) {
+     return ((x + (((fixed_point) 1) << (Fixed_Point_FBits - 1))) >> Fixed_Point_FBits);
+    }
+    return ((x - (((fixed_point) 1) << (Fixed_Point_FBits - 1))) >> Fixed_Point_FBits);
 }
--- src/lib/kernel/FixedPoint.h
+++ src/lib/kernel/FixedPoint.h
@@ -3,11 +3,15 @@
 
 #include<stdint.h>
 // representation of fixed point.
-
+#define Fixed_Point_FBits 14
 /**
  * new data type named fixed point.
  */
 typedef int32_t fixed_point;
+/**
+ * new data type named fixed point extend.
+ */
+typedef int64_t fixed_point_e;
 
 /**
  * multiplies two fixed point numbers.
@@ -17,7 +21,17 @@ typedef int32_t fixed_point;
  * divides two fixed point numbers.
  */
  fixed_point fixed_point_div(fixed_point x, fixed_point y);
-
-
-
+/**
+ * it converts from integer to fixed point and return a fixed point value.
+ */
+fixed_point integer_to_fixed_point(int x);
+/**
+ * it converts from fixed point to integer and return integer value.
+ */
+int fixed_point_to_integer(fixed_point x);
+/**
+ * rounds fixed point number to nearest integer.
+ *
+ */
+int round_fixed_point_number_to_integer(fixed_point x);
 #endif // FIXEDPOINT_INCLUDED
